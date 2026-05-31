5b732757585388125bad288df8988749f6dbe5e5
odebauche <odebauche@awegroupe.be>
2026-05-31 07:09:04 +0200

    revamp phase I

---
+++ translations/hr/common.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'common.brand' => 'KC Nalinnes',
+    'common.nav.schedule' => 'Schedule',
+    'common.nav.prices' => 'Prices',
+    'common.nav.calendar' => 'Calendar',
+    'common.nav.instructors' => 'Instructors',
+    'common.nav.documents' => 'Documents',
+    'common.nav.news' => 'News',
+    'common.nav.contact' => 'Contact',
+    'common.nav.members' => 'Members',
+    'common.theme.toggle' => 'Toggle theme',
+    'common.menu.open' => 'Open menu',
+    'common.footer.rights' => 'All rights reserved',
+    'common.footer.developed_by' => 'Developed by',
+    'common.footer.legal' => 'Legal notice',
+    'common.footer.privacy' => 'Privacy policy',
+    'common.language.label' => 'Language',
+    'common.language.fr' => 'FR',
+    'common.language.en' => 'EN',
+    'common.language.nl' => 'NL',
+];
